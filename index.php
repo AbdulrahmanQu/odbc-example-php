f4b0b2e365b94b5211df3214277c897846466aa6
AbdulrahmanQu <a.qunaibit@gmail.com>
2020-02-21 09:04:04 -0800

    initial

--- index.php
+++ index.php
@@ -1,28 +1,83 @@
 <?php
 
-$sql = 'show tables;';
+// establish connection
 
-try {
-    //$dbh = new PDO('odbc:DSN=ProdHive;');
-    $dbh = new PDO('odbc:DRIVER=Hive;Host=hive-datahub.test.env;PORT=10000;Schema=default;');
+$connection=odbc_connect('odbc','odbc','odbc');
 
-    $result = $dbh->query($sql);
 
-    $all = $result->fetchAll(PDO::FETCH_ASSOC);
-    if ($all) {
-        $keys = array_keys($all[0]);
-        foreach ($keys as $col) {
-            echo $col, "\t";
-        }
-        echo "\n\n";
-
-        foreach ($all as $row) {
-            foreach ($row as $v) {
-                echo $v, "\t";
-            }
-            echo "\n";
-        }
-    }
-} catch (PDOException $exception) {
-    echo $exception->getMessage(), "\n\n";
+if (!$connection){
+    exit("Connection Failed - ".odbc_error().":".odbc_errormsg()."\n");
 }
+
+// execute select statement
+$statement=odbc_exec($connection, "select issue_key, issue_pm from CR_WITHIN_CAD_SCOPE ");
+if (!$statement){
+    exit("Execution failed - ".odbc_error().":".odbc_errormsg()."\n");
+}
+
+// generate html code
+echo "<html><head><title>Employees</title><head><body>";
+echo "<table><tr>";
+echo "<th>CR NUMBER</th>";
+echo "<th>PM</th></tr>";
+
+// fetch data
+while (odbc_fetch_row($statement)) {
+    $issue_key=odbc_result($statement,"issue_key");
+    $issue_pm=odbc_result($statement,"issue_pm");
+
+    // output data as rows in html table
+    echo "<tr><td>$issue_key</td>";
+    echo "<td>$issue_pm</td></tr>";
+}
+
+// close connection
+odbc_close($connection);
+
+// finalize html page code
+echo "</body></table>";
+
+?>
+
+Note that the sample above generates HTML code from within a PHP script. It is also possible to embed PHP script within HTML:
+<html>
+<head>
+    <title>Employees</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>FIRST NAME</th>
+        <th>LAST NAME</th>
+    </tr>
+
+    <?php
+
+    // establish connection
+    $connection=odbc_connect('Payroll','','');
+    if (!$connection){
+        exit("Connection Failed - ".odbc_error().":".odbc_errormsg()."\n");
+    }
+
+    // execute select statement
+    $statement=odbc_exec($connection, "select first_name, last_name from employee");
+    if (!$statement){
+        exit("Execution failed - ".odbc_error().":".odbc_errormsg()."\n");
+    }
+
+    // fetch data and populate the table
+    while (odbc_fetch_row($statement)) {
+        $firstName=odbc_result($statement,"first_name");
+        $lastName=odbc_result($statement,"last_name");
+        echo "<tr><td>$firstName</td>";
+        echo "<td>$lastName</td></tr>";
+    }
+
+    // close connection
+    odbc_close($connection);
+
+    ?>
+
+</table>
+</body>
+</html>
